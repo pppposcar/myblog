337c3b6c470809e809faeeec4bb4dd0a30c0ee8b
pppposcar <Barikan1108@yahoo.co.jp>
2021-07-05 07:35:46 +0000

    first commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    template_name='blog/home.html'
